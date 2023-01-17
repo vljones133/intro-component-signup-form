60ff8f381cedcd5307db324b8c7705ad8759d589
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2023-01-17 15:00:46 -0600

    validation works!

--- script.js
+++ script.js
@@ -1,80 +1,77 @@
-// if field is empty, show icon for that field and error message
-// check email address
+// https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
 
-const submitHandler = (event) => {
+const form = document.querySelector('form');
+const input = document.getElementsByTagName('input');
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+window.addEventListener('load', () => {
+  input.className = 'valid';
+});
+
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   checkFirstNameField();
   checkLastNameField();
   checkEmailField();
   checkPasswordField();
-};
+});
 
 const checkFirstNameField = () => {
   const firstNameInput = document.getElementById('first-name-input');
-  const firstNameErrorIcon = document.getElementById('first-name-error-icon');
   const firstNameErrorMsg = document.getElementById('first-name-error-msg');
 
   if (firstNameInput.value === '') {
-    firstNameErrorIcon.style.display = 'block';
     firstNameErrorMsg.style.display = 'block';
+    firstNameInput.className = 'invalid';
   } else {
-    firstNameErrorIcon.style.display = 'none';
     firstNameErrorMsg.style.display = 'none';
   }
 };
 
 const checkLastNameField = () => {
   const lastNameInput = document.getElementById('last-name-input');
-  const lastNameErrorIcon = document.getElementById('last-name-error-icon');
   const lastNameErrorMsg = document.getElementById('last-name-error-msg');
 
   if (lastNameInput.value === '') {
-    lastNameErrorIcon.style.display = 'block';
     lastNameErrorMsg.style.display = 'block';
+    lastNameInput.className = 'invalid';
   } else {
-    lastNameErrorIcon.style.display = 'none';
     lastNameErrorMsg.style.display = 'none';
   }
 };
 
 const checkEmailField = () => {
   const emailInput = document.getElementById('email-input');
-  const emailErrorIcon = document.getElementById('email-error-icon');
   const emptyEmailErrorMsg = document.getElementById('empty-email-error-msg');
   const invalidEmailErrorMsg = document.getElementById(
     'invalid-email-error-msg'
   );
 
-  if (emailInput.value === '') {
-    emailErrorIcon.style.display = 'block';
+  if (emailInput.value.length === 0) {
     emptyEmailErrorMsg.style.display = 'block';
+    emailInput.className = 'invalid';
   } else if (
     !emailInput.value.includes('@') ||
     !emailInput.value.includes('.')
   ) {
-    emailErrorIcon.style.display = 'block';
     invalidEmailErrorMsg.style.display = 'block';
+    emailInput.className = 'invalid';
   } else {
-    emailErrorIcon.style.display = 'none';
     emptyEmailErrorMsg.style.display = 'none';
+    invalidEmailErrorMsg.style.display = 'none';
   }
 };
 
 const checkPasswordField = () => {
   const passwordInput = document.getElementById('password-input');
-  const passwordErrorIcon = document.getElementById('password-error-icon');
   const passwordErrorMsg = document.getElementById('password-error-msg');
 
   if (passwordInput.value === '') {
-    passwordErrorIcon.style.display = 'block';
     passwordErrorMsg.style.display = 'block';
+    passwordInput.className = 'invalid';
   } else {
-    passwordErrorIcon.style.display = 'none';
     passwordErrorMsg.style.display = 'none';
   }
 };
-
-// module.exports = {
-//   submitHandler,
-// };
